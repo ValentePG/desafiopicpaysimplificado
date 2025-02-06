7a6f4cd58175577416b99e7fc912cee567e50f9a
Valente <bielvhv3@gmail.com>
2025-02-06 11:09:55 -0300

    refactor: trocando id de int para long e adicionando roundingMode Half Up

--- src/main/java/dev/valente/picpaysimplificado/domain/Transaction.java
+++ src/main/java/dev/valente/picpaysimplificado/domain/Transaction.java
@@ -4,21 +4,22 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.OffsetDateTime;
 
 @Data
 @Builder
 public class Transaction {
 
-    private int id;
+    private long id;
     private BigDecimal amount;
     private Long payeeWalletId;
     private Long payerWalletId;
     private OffsetDateTime date;
 
-    public Transaction(int id, BigDecimal amount, Long payeeWalletId, Long payerWalletId, OffsetDateTime date) {
+    public Transaction(long id, BigDecimal amount, Long payeeWalletId, Long payerWalletId, OffsetDateTime date) {
         this.id = id;
-        this.amount = amount;
+        this.amount = amount.setScale(2, RoundingMode.HALF_UP);
         this.payeeWalletId = payeeWalletId;
         this.payerWalletId = payerWalletId;
         this.date = date;
