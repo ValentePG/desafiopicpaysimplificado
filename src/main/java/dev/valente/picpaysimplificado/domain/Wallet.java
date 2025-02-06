a7fa499825c74b6ba6cd0a95f7f3f5b5a01c31cf
Valente <bielvhv3@gmail.com>
2025-02-06 11:07:24 -0300

    feature: Função do enum WalletType adicionada ao construtor e wallet_type(int) adicionado para conversão facilitada pelo jdbc

--- src/main/java/dev/valente/picpaysimplificado/domain/Wallet.java
+++ src/main/java/dev/valente/picpaysimplificado/domain/Wallet.java
@@ -11,9 +11,9 @@ public class Wallet {
     private BigDecimal balance;
     private WalletType walletType;
 
-    public Wallet(long id, BigDecimal balance, int walletIntValue) {
+    public Wallet(long id, BigDecimal balance, int wallet_type) {
         this.id = id;
         this.balance = balance.setScale(2, RoundingMode.HALF_UP);
-        this.walletType = WalletType.convertToWalletType(walletIntValue);
+        this.walletType = WalletType.convertToWalletType(wallet_type);
     }
 }
