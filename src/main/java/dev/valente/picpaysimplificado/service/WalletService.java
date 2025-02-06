943ddf7f6623291864b18e9e52a7a2ee4f3b9fec
Valente <bielvhv3@gmail.com>
2025-02-06 11:06:02 -0300

    feature: @Log4j2 adicionado para futuros logs

--- src/main/java/dev/valente/picpaysimplificado/service/WalletService.java
+++ src/main/java/dev/valente/picpaysimplificado/service/WalletService.java
@@ -3,11 +3,14 @@ package dev.valente.picpaysimplificado.service;
 import dev.valente.picpaysimplificado.domain.Wallet;
 import dev.valente.picpaysimplificado.repository.WalletRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class WalletService {
@@ -19,10 +22,12 @@ public class WalletService {
     }
 
     public void updateWallets(Wallet payeeWallet, Wallet payerWallet, BigDecimal transactionAmount) {
-        var newBalanceForPayee = payeeWallet.getBalance().subtract(transactionAmount);
-        var newBalanceForPayer = payerWallet.getBalance().add(transactionAmount);
+        var newBalanceForPayee = payeeWallet.getBalance().add(transactionAmount);
+        var newBalanceForPayer = payerWallet.getBalance().subtract(transactionAmount);
+
         payeeWallet.setBalance(newBalanceForPayee);
         payerWallet.setBalance(newBalanceForPayer);
+
         walletRepository.updateWallets(payeeWallet, payerWallet);
     }
 }
