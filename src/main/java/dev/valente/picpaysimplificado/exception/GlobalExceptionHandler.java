6d3f59c7f4763ad42e52b3605e4d9b2c97db5921
Valente <bielvhv3@gmail.com>
2025-02-07 10:53:50 -0300

    feature: criando globalExceptionHandler, para tratar todas as exceções

---
+++ src/main/java/dev/valente/picpaysimplificado/exception/GlobalExceptionHandler.java
@@ -0,0 +1,55 @@
+package dev.valente.picpaysimplificado.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.OffsetDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<ApiError> handleInsufficientBalanceException(InsufficientBalanceException ex,
+                                                                       HttpServletRequest request) {
+        var apiError = ApiError.builder()
+                .timestamp(OffsetDateTime.now())
+                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .message(ex.getReason())
+                .path(request.getRequestURI())
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+    }
+
+    @ExceptionHandler(WalletTypeNotValidForTransactionException.class)
+    public ResponseEntity<ApiError> handleWalletTypeNotValidForTransactionException(WalletTypeNotValidForTransactionException ex,
+                                                                                    HttpServletRequest request) {
+        var apiError = ApiError.builder()
+                .timestamp(OffsetDateTime.now())
+                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .message(ex.getReason())
+                .path(request.getRequestURI())
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+    }
+
+    @ExceptionHandler(NotAuthorizedException.class)
+    public ResponseEntity<ApiError> handleNotAuthorizedException(NotAuthorizedException ex,
+                                                                 HttpServletRequest request) {
+        var apiError = ApiError.builder()
+                .timestamp(OffsetDateTime.now())
+                .error(HttpStatus.FORBIDDEN.getReasonPhrase())
+                .message(ex.getReason())
+                .path(request.getRequestURI())
+                .statusCode(HttpStatus.FORBIDDEN.value())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(apiError);
+    }
+}
