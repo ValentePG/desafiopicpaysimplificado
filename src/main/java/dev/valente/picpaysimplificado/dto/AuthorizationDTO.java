b55497e141db39dd91c511fd5b664e2e2a0493b9
Valente <bielvhv3@gmail.com>
2025-02-07 10:19:38 -0300

    feature: DTO de retorno da requisição

---
+++ src/main/java/dev/valente/picpaysimplificado/dto/AuthorizationDTO.java
@@ -0,0 +1,4 @@
+package dev.valente.picpaysimplificado.dto;
+
+public record AuthorizationDTO(String status, Data data) {
+}
---
+++ src/main/java/dev/valente/picpaysimplificado/dto/Data.java
@@ -0,0 +1,4 @@
+package dev.valente.picpaysimplificado.dto;
+
+public record Data(boolean authorization) {
+}
