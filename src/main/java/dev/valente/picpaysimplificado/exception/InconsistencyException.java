256edb88604d2c57b6309d68b5b55110f97e5972
Valente <bielvhv3@gmail.com>
2025-02-10 17:34:03 -0300

    refactor: renomeando exceção

--- src/main/java/dev/valente/picpaysimplificado/exception/InconsistencyException.java
+++ src/main/java/dev/valente/picpaysimplificado/exception/InconsistencyException.java
@@ -3,8 +3,8 @@ package dev.valente.picpaysimplificado.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-public class InconsistencyError extends ResponseStatusException {
-    public InconsistencyError(String message) {
+public class InconsistencyException extends ResponseStatusException {
+    public InconsistencyException(String message) {
         super(HttpStatus.BAD_REQUEST, message);
     }
 }
