f27a8cf68369864b5b3bf12c838ac43d2fd335fa
Valente <bielvhv3@gmail.com>
2025-02-10 17:37:05 -0300

    feature: Exceção criada para serviço de notificação

---
+++ src/main/java/dev/valente/picpaysimplificado/exception/UnnavailableServiceException.java
@@ -0,0 +1,7 @@
+package dev.valente.picpaysimplificado.exception;
+
+public class UnnavailableServiceException extends RuntimeException {
+    public UnnavailableServiceException(String message) {
+        super(message);
+    }
+}
