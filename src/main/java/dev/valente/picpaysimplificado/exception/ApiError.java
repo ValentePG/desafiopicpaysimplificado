62f544b2c4e61282bccf5c35cc8fc59900eb9d1d
Valente <bielvhv3@gmail.com>
2025-02-10 17:33:35 -0300

    refactor: Falta do getter ocasionava em erro na formatação do OffSetDateTime

--- src/main/java/dev/valente/picpaysimplificado/exception/ApiError.java
+++ src/main/java/dev/valente/picpaysimplificado/exception/ApiError.java
@@ -1,10 +1,12 @@
 package dev.valente.picpaysimplificado.exception;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.OffsetDateTime;
 
 @Builder
+@Getter
 public class ApiError {
     private OffsetDateTime timestamp;
     private int statusCode;
