9243dddcebbb42f5b69ce0bf33c36cd2249a4e19
Valente <bielvhv3@gmail.com>
2025-02-10 17:39:24 -0300

    refactor: renomeando exceção no service

--- src/main/java/dev/valente/picpaysimplificado/service/WalletService.java
+++ src/main/java/dev/valente/picpaysimplificado/service/WalletService.java
@@ -1,7 +1,7 @@
 package dev.valente.picpaysimplificado.service;
 
 import dev.valente.picpaysimplificado.domain.Wallet;
-import dev.valente.picpaysimplificado.exception.InconsistencyError;
+import dev.valente.picpaysimplificado.exception.InconsistencyException;
 import dev.valente.picpaysimplificado.repository.WalletRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -34,7 +34,7 @@ public class WalletService {
 
     private void updateWallet(Wallet wallet) {
         var rowsAffected = walletRepository.updateWallet(wallet);
-        if(rowsAffected == 0) throw new InconsistencyError("Erro de inconsistência na transação. Wallet ID: "
+        if(rowsAffected == 0) throw new InconsistencyException("Erro de inconsistência na transação. Wallet ID: "
                 + wallet.getId());
         wallet.setVersion(wallet.getVersion() + 1);
     }
