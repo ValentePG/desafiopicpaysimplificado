93c4ffb771130654d373d7f59db81e1f9bbae70b
Valente <bielvhv3@gmail.com>
2025-02-11 21:26:37 -0300

    feature: Configuração do swagger

---
+++ src/main/java/dev/valente/picpaysimplificado/config/OpenApiConfig.java
@@ -0,0 +1,10 @@
+package dev.valente.picpaysimplificado.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "PicPay Simplificado", version = "1.0"))
+public class OpenApiConfig {
+}
