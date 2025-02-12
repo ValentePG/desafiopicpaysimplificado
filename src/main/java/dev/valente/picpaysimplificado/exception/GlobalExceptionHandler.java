a01799f14f640ec555441f52774eeabea7d79bea
Valente <bielvhv3@gmail.com>
2025-02-11 21:26:18 -0300

    feature: Exceção personalizada para tratar as exceções do bean validation

--- src/main/java/dev/valente/picpaysimplificado/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/valente/picpaysimplificado/exception/GlobalExceptionHandler.java
@@ -1,12 +1,16 @@
 package dev.valente.picpaysimplificado.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,6 +25,29 @@ public class GlobalExceptionHandler {
                 .path(request.getRequestURI())
                 .statusCode(HttpStatus.BAD_REQUEST.value())
                 .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleMethodArgumentNotValid(MethodArgumentNotValidException e,
+                                                                 HttpServletRequest request) {
+        var defaultMessage = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .filter(Objects::nonNull)
+                .sorted()
+                .collect(Collectors.joining(", "));
+
+
+        var apiError = ApiError.builder()
+                .timestamp(OffsetDateTime.now())
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .message(defaultMessage)
+                .path(request.getRequestURI())
+                .build();
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
     }
 
