68f18c8f2ad1df4b0136df64630a0e253ac655ef
Valente <bielvhv3@gmail.com>
2025-02-13 20:20:17 -0300

    feature: Configuração do rabbitMQ container

--- src/test/java/dev/valente/picpaysimplificado/config/TestcontainersConfiguration.java
+++ src/test/java/dev/valente/picpaysimplificado/config/TestcontainersConfiguration.java
@@ -1,11 +1,17 @@
 package dev.valente.picpaysimplificado.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.RabbitMQContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -26,4 +32,21 @@ public class TestcontainersConfiguration {
         return new RabbitMQContainer(DockerImageName.parse("rabbitmq:4-management"))
                 .withExposedPorts(5672, 15672);
     }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(RabbitTemplate rabbitTemplate) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(rabbitTemplate);
+
+        Queue directExchangeQueue = new Queue("direct_queue", false);
+        rabbitAdmin.declareQueue(directExchangeQueue);
+
+        DirectExchange directExchange = new DirectExchange("direct_exchange");
+        rabbitAdmin.declareExchange(directExchange);
+
+        Binding binding = BindingBuilder.bind(directExchangeQueue).to(directExchange)
+                .with("direct_routingKey");
+        rabbitAdmin.declareBinding(binding);
+        return rabbitAdmin;
+    }
+
 }
