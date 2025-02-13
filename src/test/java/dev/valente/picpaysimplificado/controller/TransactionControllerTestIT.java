691bd5f764bedf5d3a45fa5f4ca217970c974462
Valente <bielvhv3@gmail.com>
2025-02-13 20:20:50 -0300

    feature: Inicio dos testes de integração do controller

--- src/test/java/dev/valente/picpaysimplificado/controller/TransactionControllerTestIT.java
+++ src/test/java/dev/valente/picpaysimplificado/controller/TransactionControllerTestIT.java
@@ -2,16 +2,32 @@ package dev.valente.picpaysimplificado.controller;
 
 import dev.valente.picpaysimplificado.config.RestAssuredConfig;
 import dev.valente.picpaysimplificado.config.TestContainers;
+import dev.valente.picpaysimplificado.exception.NotAuthorizedException;
+import dev.valente.picpaysimplificado.repository.TransactionRepository;
+import dev.valente.picpaysimplificado.service.AuthorizationService;
+import dev.valente.picpaysimplificado.service.NotifyConsumer;
+import dev.valente.picpaysimplificado.service.NotifyService;
+import dev.valente.picpaysimplificado.utils.FileUtils;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import net.javacrumbs.jsonunit.assertj.JsonAssertions;
+import org.assertj.core.api.Assertions;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.*;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = RestAssuredConfig.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TransactionControllerTestIT extends TestContainers {
 
     public final String URL = "/v1/transfer";
@@ -19,24 +35,95 @@ class TransactionControllerTestIT extends TestContainers {
     @Autowired
     private RequestSpecification requestSpecification;
 
+    @Autowired
+    private FileUtils fileUtils;
+
+    @MockitoSpyBean
+    private NotifyConsumer notifyConsumer;
+
+    @MockitoSpyBean
+    private TransactionRepository transactionRepository;
+
+    @MockitoSpyBean
+    private AuthorizationService authorizationService;
+
+    @MockitoSpyBean
+    private NotifyService notifyService;
+
     @BeforeEach
     void setUp() {
         RestAssured.requestSpecification = requestSpecification;
     }
 
     @Test
-    void createTransaction() {
+    @Order(1)
+    @Sql(value = "/sql/initthreewallets.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/dropdata.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @DisplayName("POST v1/transfer should create a transaction with success when all is ok")
+    void createTransaction_ShouldCreateTransaction_WhenAllIsOk() throws IOException {
+
+        BDDMockito.doNothing().when(authorizationService).getAuthorization();
+//        BDDMockito.doNothing().when(notifyService).notify(BDDMockito.any(Transaction.class));
+
+
+        var request = fileUtils.readFile("/transaction/post_createtransactionrequest_200.json");
+        var responseFromFile = fileUtils.readFile("/transaction/post_createtransactionresponse_200.json");
+
         var response = RestAssured.given()
                 .contentType(ContentType.JSON).accept(ContentType.JSON)
-                .body("")
+                .body(request)
                 .when()
                 .post(URL)
                 .then()
-                .statusCode(HttpStatus.BAD_REQUEST.value())
-                .log().all()
-                .extract().response().body().asString();
+                .statusCode(HttpStatus.OK.value())
+                .body(Matchers.equalTo(responseFromFile))
+                .log().all();
 
-        System.out.println(response);
+
+        var transactions = transactionRepository.getAllTransactions();
+
+        Assertions.assertThat(transactions)
+                .isNotEmpty()
+                .hasSize(1);
+
+        var transactionCreated = transactions.getFirst();
+
+        Assertions.assertThat(transactionCreated)
+                .matches(t -> t.getPayeeWalletId().equals(2L))
+                .matches(t -> t.getPayerWalletId().equals(1L))
+                .matches(t -> t.getAmount().equals(BigDecimal.valueOf(50.0)
+                        .setScale(2, RoundingMode.HALF_UP)));
+
+
+    }
+
+    @Test
+    @Order(2)
+    @Sql(value = "/sql/initthreewallets.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/dropdata.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @DisplayName("POST v1/transfer should create a transaction with success when all is ok")
+    void createTransaction_ShouldReturnNotAuthorizedException_WhenTransactionIsNotAuthorized() {
+
+
+        BDDMockito.doThrow(new NotAuthorizedException("Authorization[status=fail, data=Data[authorization=false]]"))
+                .when(authorizationService).getAuthorization();
+
+        var request = fileUtils.readFile("/transaction/post_createtransactionrequest_200.json");
+        var responseFile = fileUtils.readFile("/transaction/post_createtransactionnotauthorized_403.json");
+
+        var response = RestAssured.given()
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .body(request)
+                .when()
+                .post(URL)
+                .then()
+                .statusCode(HttpStatus.FORBIDDEN.value())
+                .log().all()
+                .extract().response().asString();
+
+        JsonAssertions.assertThatJson(response)
+                .whenIgnoringPaths("timestamp")
+                .isEqualTo(responseFile);
 
     }
 }
