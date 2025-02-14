94ada271d1a9a318bbb38b323846ffd689e8e4f5
Valente <bielvhv3@gmail.com>
2025-02-14 10:10:56 -0300

    feature: Testes de validação dos dados que entram no controller

--- src/test/java/dev/valente/picpaysimplificado/controller/TransactionControllerTestIT.java
+++ src/test/java/dev/valente/picpaysimplificado/controller/TransactionControllerTestIT.java
@@ -2,6 +2,7 @@ package dev.valente.picpaysimplificado.controller;
 
 import dev.valente.picpaysimplificado.config.RestAssuredConfig;
 import dev.valente.picpaysimplificado.config.TestContainers;
+import dev.valente.picpaysimplificado.domain.Transaction;
 import dev.valente.picpaysimplificado.exception.NotAuthorizedException;
 import dev.valente.picpaysimplificado.repository.TransactionRepository;
 import dev.valente.picpaysimplificado.service.AuthorizationService;
@@ -15,6 +16,9 @@ import net.javacrumbs.jsonunit.assertj.JsonAssertions;
 import org.assertj.core.api.Assertions;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,6 +29,7 @@ import org.springframework.test.context.jdbc.Sql;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.stream.Stream;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = RestAssuredConfig.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -63,7 +68,7 @@ class TransactionControllerTestIT extends TestContainers {
     void createTransaction_ShouldCreateTransaction_WhenAllIsOk() throws IOException {
 
         BDDMockito.doNothing().when(authorizationService).getAuthorization();
-//        BDDMockito.doNothing().when(notifyService).notify(BDDMockito.any(Transaction.class));
+        BDDMockito.doNothing().when(notifyService).notify(BDDMockito.any(Transaction.class));
 
 
         var request = fileUtils.readFile("/transaction/post_createtransactionrequest_200.json");
@@ -101,7 +106,7 @@ class TransactionControllerTestIT extends TestContainers {
     @Order(2)
     @Sql(value = "/sql/initthreewallets.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(value = "/sql/dropdata.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    @DisplayName("POST v1/transfer should create a transaction with success when all is ok")
+    @DisplayName("POST v1/transfer should return NotAuthorized exception when is not authorized")
     void createTransaction_ShouldReturnNotAuthorizedException_WhenTransactionIsNotAuthorized() {
 
 
@@ -125,5 +130,48 @@ class TransactionControllerTestIT extends TestContainers {
                 .whenIgnoringPaths("timestamp")
                 .isEqualTo(responseFile);
 
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameterizedValidationOfData")
+    @Order(3)
+    @DisplayName("POST v1/transfer should return BAD REQUEST when data is invalid")
+    @Sql(value = "/sql/initthreewallets.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/dropdata.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void createTransaction_ShouldReturnBADRequest_WhenDataIsInvalid(String path, String responseFile) {
+
+        var request = fileUtils.readFile(path);
+
+        var responseFromFile = fileUtils.readFile(responseFile);
+
+        var response = RestAssured.given()
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .body(request)
+                .when()
+                .post(URL)
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .log().all()
+                .extract().response().asString();
+
+        JsonAssertions.assertThatJson(response)
+                .whenIgnoringPaths("timestamp")
+                .isEqualTo(responseFromFile);
+    }
+
+    private static Stream<Arguments> parameterizedValidationOfData() {
+
+        var request1 = "/transaction/post_createtransactionwithdatainvalid1_400.json";
+        var request2 = "/transaction/post_createtransactionwithdatainvalid2_400.json";
+        var request3 = "/transaction/post_createtransactionrequest2_200.json";
+        var response1 = "/transaction/post_createtransactionargumentnotvalid1_400.json";
+        var response2 = "/transaction/post_createtransactionargumentnotvalid2_400.json";
+        var response3 = "/transaction/post_createtransactioninsufficientbalance_400.json";
+
+        return Stream.of(
+                Arguments.of(request1, response1),
+                Arguments.of(request2, response2),
+                Arguments.of(request3, response3)
+        );
     }
 }
