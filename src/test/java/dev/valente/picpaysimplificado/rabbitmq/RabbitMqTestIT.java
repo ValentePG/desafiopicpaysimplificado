9e5a7ad59ed2899a5b39258e6f7945f8c2ea53bc
Valente <bielvhv3@gmail.com>
2025-02-15 12:24:31 -0300

    feature: Teste do producer e do consumidor das mensagens no RabbitMQ

---
+++ src/test/java/dev/valente/picpaysimplificado/rabbitmq/RabbitMqTestIT.java
@@ -0,0 +1,67 @@
+package dev.valente.picpaysimplificado.rabbitmq;
+
+import dev.valente.picpaysimplificado.config.TestContainers;
+import dev.valente.picpaysimplificado.domain.Transaction;
+import dev.valente.picpaysimplificado.service.NotifyClient;
+import dev.valente.picpaysimplificado.service.NotifyConsumer;
+import dev.valente.picpaysimplificado.service.NotifyService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.mockito.ArgumentCaptor;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.web.client.RestClient;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.concurrent.TimeUnit;
+
+import static dev.valente.picpaysimplificado.utils.TransactionUtils.*;
+import static org.awaitility.Awaitility.await;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class RabbitMqTestIT extends TestContainers {
+
+    @Autowired
+    private NotifyService notifyService;
+
+    @MockitoSpyBean
+    private NotifyConsumer notifyConsumer;
+
+    @MockitoSpyBean
+    private NotifyClient notifyClient;
+
+    @Test
+    @Order(1)
+    @DisplayName("notify() should deliver a message when successfull")
+    void notify_ShouldDeliverAmessage_WhenSuccessfull() {
+
+        notifyService.notify(AFTER_TRANSACTION_SUCCESS);
+
+        var timestamp = AFTER_TRANSACTION_SUCCESS.getDate();
+
+        ArgumentCaptor<Transaction> captor = ArgumentCaptor.forClass(Transaction.class);
+
+        BDDMockito.doNothing().when(notifyClient).pushNotification();
+
+        await().atMost(10, TimeUnit.SECONDS).untilAsserted(() ->
+                BDDMockito.verify(notifyConsumer).receive(captor.capture()));
+
+
+        var receivedTransaction = captor.getValue();
+
+        Assertions.assertThat(receivedTransaction)
+                .matches(t -> t.getId() == 1L)
+                .matches(t -> t.getPayeeWalletId().equals(2L))
+                .matches(t -> t.getPayerWalletId().equals(1L))
+                .matches(t -> t.getAmount().equals(BigDecimal.valueOf(50.0)
+                        .setScale(2, RoundingMode.HALF_UP)))
+                .matches(t -> t.getDate().equals(timestamp));
+    }
+}
